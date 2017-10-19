5b1c22c4ee1fd1d8caa874924d0effcfba9d3a71
Niek Palm <dev.npalm@gmail.com>
2017-10-19 13:36:06 +0200

    clean up

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -1,6 +1,5 @@
 package demo;
 
-//import com.oembedler.moon.graphql.boot.SchemaParserDictionary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args); //NOSONAR
+        SpringApplication.run(Application.class, args);
     }
 
 }
