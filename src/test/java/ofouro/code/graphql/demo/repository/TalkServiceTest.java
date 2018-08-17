04a409769e4318fd0106817bed6ff923ece5dd7a
Niek Palm <dev.npalm@gmail.com>
2018-08-18 00:30:39 +0200

    Add tests

---
+++ src/test/java/ofouro/code/graphql/demo/repository/TalkServiceTest.java
@@ -0,0 +1,70 @@
+package ofouro.code.graphql.demo.repository;
+
+import ofouro.code.graphql.demo.model.Talk;
+import ofouro.code.graphql.demo.service.TalkRepository;
+import ofouro.code.graphql.demo.service.TalkService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.ArrayList;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class TalkServiceTest {
+
+    @Autowired
+    private TalkService talkService;
+
+    @Autowired
+    private TalkRepository talkRepository;
+
+    private Talk aTalk = new Talk("GraphQL is Cool", "summary", new ArrayList<>(), new ArrayList<>());
+
+    @Test
+    public void shouldFindTalkLikeNameMiddle() throws Exception {
+        checkFindTalk("L is Coo");
+    }
+
+    @Test
+    public void shouldFindTalkLikeNameUPPER() throws Exception {
+        checkFindTalk("IS COOL");
+    }
+
+    @Test
+    public void shouldNotFind() throws Exception {
+        checkNotFindTalk("REST");
+    }
+
+
+    private Iterable<Talk> setupAndFind(String title) {
+        // Arrange
+        talkRepository.save(aTalk);
+
+        // Act
+        return talkService.find(new Talk(title, null, null, null));
+    }
+
+    private void checkFindTalk(String title) {
+        // Arrange && Act
+        Iterable<Talk> findTalk = setupAndFind(title);
+
+        // Assert
+        assertThat(aTalk, equalTo(findTalk.iterator().next()));
+    }
+
+    private void checkNotFindTalk(String title) {
+        // Arrange && Act
+        Iterable<Talk> findTalk = setupAndFind(title);
+
+        // Assert
+        assertThat(Boolean.FALSE, equalTo(findTalk.iterator().hasNext()));
+    }
+
+}
