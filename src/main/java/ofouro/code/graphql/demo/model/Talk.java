139a05e2d09e200af481b34773b2ce76c6978166
Niek Palm <dev.npalm@gmail.com>
2018-08-18 00:37:08 +0200

    Fix tests

--- src/main/java/ofouro/code/graphql/demo/model/Talk.java
+++ src/main/java/ofouro/code/graphql/demo/model/Talk.java
@@ -13,7 +13,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(exclude = {"id"})
+@EqualsAndHashCode(exclude = {"id", "speakers", "conferences"})
 public class Talk {
 
     @Id
