7814b4f52b465d9d6e55a874a617a4f9a249ef09
Niek Palm <dev.npalm@gmail.com>
2019-03-10 17:29:31 +0100

    Fix warning

--- src/main/java/ofouro/code/graphql/demo/resolvers/CommentPageableResponse.java
+++ src/main/java/ofouro/code/graphql/demo/resolvers/CommentPageableResponse.java
@@ -1,12 +1,14 @@
 package ofouro.code.graphql.demo.resolvers;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import ofouro.code.graphql.demo.model.Comment;
 import org.springframework.data.domain.Page;
 
 import java.util.List;
 
 @Data
+@EqualsAndHashCode
 public class CommentPageableResponse extends PageableResponse {
 
     private List<Comment> content;
